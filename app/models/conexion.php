599a3c4ca50254caf6029c909985dcc0f8fb8875
AgusCiro <agusego13@gmail.com>
2025-10-10 17:49:11 -0300

    Mi comit

--- app/models/conexion.php
+++ app/models/conexion.php
@@ -1,21 +1,31 @@
 <?php
 
+
 function conectar()
 {
     $conexion = null;
-    $dns= 'odbc:gebipadesa';
-    $db= '???';
-    $user='gebipa';
-    $pwd='gebipa';
-    
-    try
-    {
-        $conexion = new PDO($dns,$user,$pwd);
-    }
-    catch(PDOException $e)
-    {
-        echo 'Error al conectar con la bd'.$e;
+
+
+    // Datos de conexión
+    $host = '127.0.0.1';     // o localhost
+    $db   = 'CUERPO_SANO';   // nombre de la base que creaste en phpMyAdmin
+    $user = 'root';          // usuario por defecto en XAMPP
+    $pwd  = '';              // contraseña vacía por defecto en XAMPP
+    $charset = 'utf8';
+
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
+    try {
+        $conexion = new PDO($dsn, $user, $pwd);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'Error al conectar con la base de datos: ' . $e->getMessage();
         exit;
     }
+
+
     return $conexion;
 }
+?>
